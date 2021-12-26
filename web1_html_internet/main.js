2edc4c437ed68b2e193df0db3d3e3e68f1cb9190
Jay <jaewhee@tictag.io>
2021-12-27 02:55:30 +0900

    211227

--- web1_html_internet/main.js
+++ web1_html_internet/main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
         <!doctype html>
         <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
         <body>
           <h1><a href="/">WEB</a></h1>
           ${list}
-          <a href="/create">create</a>
+          ${control}
           ${body}
         </body>
         </html>
@@ -49,6 +49,7 @@ var app = http.createServer(function (request, response) {
           title,
           list,
           `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`,
         );
         response.writeHead(200);
         response.end(template);
@@ -65,6 +66,7 @@ var app = http.createServer(function (request, response) {
               title,
               list,
               `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
             );
             response.writeHead(200);
             response.end(template);
@@ -80,7 +82,7 @@ var app = http.createServer(function (request, response) {
         title,
         list,
         `
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder = "title"></p>
         <p>
           <textarea name="description" placeholder = "description"></textarea>
@@ -90,6 +92,7 @@ var app = http.createServer(function (request, response) {
         </p>
       </form>
       `,
+        '',
       );
       response.writeHead(200);
       response.end(template);
@@ -101,12 +104,60 @@ var app = http.createServer(function (request, response) {
     });
     request.on('end', function () {
       var post = qs.parse(body);
-      console.log(post);
       var title = post.title;
       var description = post.description;
+      fs.writeFile(`../data/${title}`, description, 'utf8', function (err) {
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end('success');
+      });
     });
-    response.writeHead(200);
-    response.end('success');
+  } else if (pathname === '/update') {
+    fs.readdir('../data', function (error, filelist) {
+      fs.readFile(
+        `../data/${queryData.id}`,
+        'utf8',
+        function (err, description) {
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(
+            title,
+            list,
+            `
+            <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder = "title" value = ${title}></p>
+            <p>
+              <textarea name="description" placeholder = "description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+          );
+          response.writeHead(200);
+          response.end(template);
+        },
+      );
+    });
+  } else if (pathname === '/update_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`../data/${id}`, `../data/${title}`, function (err) {
+        fs.writeFile(`../data/${title}`, description, 'utf8', function (err) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end('success');
+        });
+      });
+    });
   } else {
     response.writeHead(404);
     response.end('Not found');
